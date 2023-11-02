f29f4a611d63bd6ca174d8122d4cf408dbbe6da3
Sheens507 <85970198+Sheens507@users.noreply.github.com>
2023-11-02 20:46:48 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,15 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var contact = document.getElementById("contact");
+    var memberBottom = document.getElementById("member-bottom");
+    var skillsBottom = document.getElementById("skills-bottom");
+    var contactBottom = document.getElementById("contact-bottom");
+
+    member.style.display = "block";
+    skills.style.display = "none";
+    contact.style.display = "none";
+    memberBottom.style.display = "block";
+    skillsBottom.style.display = "none";
+    contactBottom.style.display = "none";
+}
